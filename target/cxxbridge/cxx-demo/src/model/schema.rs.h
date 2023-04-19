de1b43676c9f99b6135ab48fcb84fc403daa04af
Jan Kaul <jankaul@mailbox.org>
2023-04-19 18:28:52 +0200

    add target

---
+++ target/cxxbridge/cxx-demo/src/model/schema.rs.h
@@ -0,0 +1,1 @@
+/home/jan/workspace/rust/cxx-demo/target/debug/build/cxx-demo-360d7dcb532eedf7/out/cxxbridge/include/cxx-demo/src/model/schema.rs.h
---
+++ target/cxxbridge/cxx-demo/src/table/mod.rs.h
@@ -0,0 +1,1 @@
+/home/jan/workspace/rust/cxx-demo/target/debug/build/cxx-demo-360d7dcb532eedf7/out/cxxbridge/include/cxx-demo/src/table/mod.rs.h
